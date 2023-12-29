a2070d69fb45d0dafcd82704967b0ea2109e6fab
YabseraBogale <yabserabogalercd30932013@gmail.com>
2023-12-29 19:26:50 +0300

    ok

--- inital-setup/Register.cs
+++ inital-setup/Register.cs
@@ -1,3 +1,11 @@
 public class Register{
+        public string username;
+        public string email;
 
+        protected string password;
+
+        protected string firstname;
+        protected string middlename;
+        protected string lastname;
+        
 }
