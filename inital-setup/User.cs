1746e4ec1b2102304bbec443b42dd8f8412e8488
YabseraBogale <yabserabogalercd30932013@gmail.com>
2023-12-30 19:17:36 +0300

    ok

--- inital-setup/User.cs
+++ inital-setup/User.cs
@@ -1,3 +1,10 @@
 public class User{
-    
+    public string username;
+    public string email;
+    protected string password;
+
+    protected string firstname;
+    protected string middlename;
+    protected string lastname;
+        
 }
